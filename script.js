d50087c23842ab79fe683a381521399f3eecf70c
caioriegert <caioriegert@gmail.com>
2024-05-26 22:35:38 -0300

    parte lógica da calculadora está funcionando

--- script.js
+++ script.js
@@ -18,6 +18,18 @@ const rpmTorque02 = document.getElementById('rpm-torque-carro02');
 
 const botaoResultado = document.getElementById('botao-comparar');
 
+let pesoXpotencia01;
+let conversaoCm01;
+let rpmXtorque01;
+let mediaGeral01;
+
+let pesoXpotencia02;
+let conversaoCm02;
+let rpmXtorque02;
+let mediaGeral02;
+
+
+/*
 function contaCarro(potencia, peso, larguraPneu, aroPneu, alturaPneu, torque, rpm){
     const pesoXpotencia = peso / potencia;
     function diametroPneu(){
@@ -36,13 +48,42 @@ function contaCarro01(){
 function contaCarro02(){
     return contaCarro(potencia02, peso02, larguraPneu02, aroPneu02, alturaPneu02, torque02, rpmTorque02)
 }
+*/
 
-botaoResultado.onclick = contaCarro01;
+function contaCarro01(){
+    pesoXpotencia01 = peso01.value / potencia01.value;
+    console.log(pesoXpotencia01);
+    conversaoCm01 = (((aroPneu01.value * 2.54) + (2 * (((larguraPneu01.value / 10) * (alturaPneu01.value)) / 100)))) / 10;
+    console.log(conversaoCm01);
+    rpmXtorque01 = (rpmTorque01.value / torque01.value) / 10;
+    console.log(rpmXtorque01);
+    mediaGeral01 = (pesoXpotencia01 + conversaoCm01 + rpmXtorque01) / 3;
+    return mediaGeral01;
+}
+
+function contaCarro02(){
+    pesoXpotencia02 = peso02.value / potencia02.value;
+    console.log(pesoXpotencia02);
+    conversaoCm02 = (((aroPneu02.value * 2.54) + (2 * (((larguraPneu02.value / 10) * (alturaPneu02.value)) / 100)))) / 10;
+    console.log(conversaoCm02);
+    rpmXtorque02 = (rpmTorque02.value / torque02.value) / 10;
+    console.log(rpmXtorque02);
+    mediaGeral02 = (pesoXpotencia02 + conversaoCm02 + rpmXtorque02) / 3;
+    return mediaGeral02;
+}
 
 function resultado(){
-if (contaCarro01 > contaCarro02){
-   alert("o carro 01 é lento")
-} else if(contaCarro01 < contaCarro02){
-    alert("o carro 02 é lento")
+    if (contaCarro01() > contaCarro02()){
+        alert("o carro " + carro01.value + " perderia este racha.")
+    } else if(contaCarro01() < contaCarro02()){
+        alert("o carro " + carro02.value + " perderia este racha.")
+    }
 }
+    
+botaoResultado.onclick = function(){
+    contaCarro01();
+    console.log("A nota do carro " + carro01.value + " é " + mediaGeral01);
+    contaCarro02();
+    console.log("A nota do carro " + carro02.value + " é " + mediaGeral02);
+    resultado();
 }
